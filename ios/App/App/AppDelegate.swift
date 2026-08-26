295a7c067b7a9960de744ce6c64ea65c8262c15a
codercee90 <codercee@gmail.com>
2026-08-26 10:39:08 +0700

    Update AppDelegate.swift

--- ios/App/App/AppDelegate.swift
+++ ios/App/App/AppDelegate.swift
@@ -104,12 +104,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
         self.cachedApnsToken = apnsToken // Lưu lại
     
         if isFirebaseAllowed {
-            // Messaging.messaging().apnsToken = deviceToken
-            #if DEBUG
-            Messaging.messaging().setApnsToken(deviceToken, type: .sandbox)
-            #else
-            Messaging.messaging().setApnsToken(deviceToken, type: .prod)
-            #endif
+            Messaging.messaging().apnsToken = deviceToken
             NotificationCenter.default.post(name: .capacitorDidRegisterForRemoteNotifications, object: deviceToken)
         }
     
